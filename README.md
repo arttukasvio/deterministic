d5aa7cb54c88b0cd6d7624613b9ceeae1ccfb162
Arttu Kasvio <arttu@ubuntu.(none)>
2014-02-28 16:38:05 -0500

    Working deterministic gpg key generation!

--- README.md
+++ README.md
@@ -14,7 +14,28 @@ Requirements
 
 * [GPG](http://gnupg.org)
 * [Electrum](https://electrum.org)
+* [MonkeySphere](http://web.monkeysphere.info/)
+* [PyCrypto](https://www.dlitz.net/software/pycrypto/)
 
 How to use
 ----------
-TODO
+
+    python deterministicgpg.py
+
+Then enter Name, Email, and electrum style seed.
+
+Issues
+------
+
+1. Is my deterministic random number generator cryptographically secure?
+2. Is PyCrypto's RSA key generation correct?
+
+Future
+------
+
+* Electrum plugin built into GUI
+* Option to generate keys off of electrum private keys (is this helpful in any way?)
+* Non-RSA keys?
+* Set trust on generated gpg keys
+* Anything else we should deterministically generate? Bit-message IDs?
+* 
---
+++ deterministicgpg.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+#  Copyright 2014 Arttu Kasvio <arttu@ubuntu>
+#  
+# This is free and unencumbered software released into the public domain.
+# See LICENSE for more info and refer to <http://unlicense.org>
+
+# Requirements: electrum, pycrypto, monkeysphere, gpg
+
+import os
+import getpass
+import hashlib
+import electrum
+import subprocess
+from Crypto.PublicKey import RSA
+
+class DeterministicRandom(object):
+  """ Lame, probably unsafe deterministic "random" generator
+      Somewhat inspired by: http://blog.cr.yp.to/20140205-entropy.html
+      Some code from: https://github.com/dlitz/pycrypto/blob/master/lib/Crypto/Random/_UserFriendlyRNG.py
+  """
+
+  def __init__(self, seed):
+    self.closed = False
+    self.seed = seed
+    self.iteration = 0
+    self.data = ""
+
+  def close(self):
+    self.closed = True
+
+  def flush(self):
+    pass
+
+  def read(self, N):
+    """Return N bytes from the RNG."""
+    if self.closed:
+      raise ValueError("I/O operation on closed file")
+    if not isinstance(N, (long, int)):
+      raise TypeError("an integer is required")
+    if N < 0:
+      raise ValueError("cannot read to end of infinite stream")
+
+    # Create the "random" data
+    while len(self.data) < N:
+      self.data += hashlib.sha512(seed + str(self.iteration)).digest()
+      self.iteration += 1
+
+    retval = self.data[:N]
+    self.data = self.data[N:]
+    assert len(retval) == N
+
+    return retval
+
+if __name__ == '__main__':
+  # get name/email for GPG id
+  name = raw_input('Name: ')
+  email= raw_input('Email: ')
+
+  # Example seeds for test:
+  #   action draw bit shove single however shore language visit wonderful swell pale
+  #   motion shut tool sadness focus scratch wash match torture tightly situation jump
+  seed = getpass.getpass(prompt="Seed: ", stream=None)
+  seed = electrum.mnemonic_decode(seed.split())
+
+  rand = DeterministicRandom(seed)
+
+  key = RSA.generate(4096, rand.read)
+
+  # Since we're auto-generating the key default the creation time to UNIX time 0
+  os.environ['PEM2OPENPGP_TIMESTAMP'] = '0'
+
+  #pem2openpgp "Foo Bar <fbar@linux.net>" < priv.pem | gpg --import
+  pem2openpgp = subprocess.Popen(['pem2openpgp', '%s <%s>' % (name, email)],
+      stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+  gpg_id = pem2openpgp.communicate(key.exportKey(pkcs=1))[0]
+
+  gpg_import = subprocess.Popen(['gpg', '--import'], stdin=subprocess.PIPE)
+  gpg_import.communicate(gpg_id)
+
